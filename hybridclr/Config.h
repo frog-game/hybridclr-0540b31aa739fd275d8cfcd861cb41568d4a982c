7fb88b02bfd9f2173dc9b3e55239d80b96cb441c
frog-game <31720616+frog-game@users.noreply.github.com>
2022-12-04 22:43:49 +0800

    first commit

---
+++ hybridclr/Config.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "CommonDef.h"
+
+namespace hybridclr
+{
+	/// @brief 配置
+	class Config
+	{
+
+	public:
+		Config()
+		{
+			_threadObjectStackSize = 1024 * 128;//线程堆栈大小 单位字节
+			_threadFrameStackSize = 1024 * 2;//线程栈帧大小 单位字节
+			_threadExceptionFlowSize = 512;//线程异常流大小 单位字节
+		}
+
+		static Config& GetIns()
+		{
+			return s_ins;
+		}
+
+		uint32_t GetInterpreterThreadObjectStackSize()
+		{
+			return _threadObjectStackSize;
+		}
+
+		void SetInterpreterThreadObjectStackSize(uint32_t count)
+		{
+			_threadObjectStackSize = count;
+		}
+
+		uint32_t GetInterpreterThreadFrameStackSize()
+		{
+			return _threadFrameStackSize;
+		}
+
+		void SetInterpreterThreadFrameStackSize(uint32_t count)
+		{
+			_threadFrameStackSize = count;
+		}
+
+		uint32_t GetInterpreterThreadExceptionFlowSize()
+		{
+			return _threadExceptionFlowSize;
+		}
+
+		void SetInterpreterThreadExceptionFlowSize(uint32_t count)
+		{
+			_threadExceptionFlowSize = count;
+		}
+
+	private:
+		static Config s_ins;
+
+	private:
+		uint32_t _threadObjectStackSize;
+		uint32_t _threadFrameStackSize;
+		uint32_t _threadExceptionFlowSize;
+	};
+}
+
+
+
